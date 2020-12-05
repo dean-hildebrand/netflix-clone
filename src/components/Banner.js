f2f55c075a239788a5d22fb879bc65e8a07302a8
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-12-05 08:44:18 -0500

    Added buttons and movie title to banner movie

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -14,17 +14,28 @@ function Banner() {
           Math.floor(Math.random() * request.data.results.length - 1)
         ]
       );
-
       return request;
     }
     fetchData();
   }, []);
 
-  console.log(movie);
+
 
   return (
-    <header>
-      <h1>Movies</h1>
+    <header className="banner"
+    style={{
+      backgroundSize: "cover",
+      backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+      backgroundPosition: "center center"
+    }}
+    >
+      <div className="banner__content">
+      <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+      <div className="banner__buttons">
+      <button className="banner__button">Play</button>
+      <button className="banner__button">My List</button>
+            </div>
+      </div>
     </header>
   );
 }
